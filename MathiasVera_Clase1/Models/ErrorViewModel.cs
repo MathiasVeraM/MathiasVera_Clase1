efaa9e3aeac43d926c5131021cd3d7dc92bdd95f
MathiasVeraM <mathias.vera@udla.edu.ec>
2024-09-26 08:07:08 -0500

    Agregar archivos de proyecto.

---
+++ MathiasVera_Clase1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MathiasVera_Clase1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
